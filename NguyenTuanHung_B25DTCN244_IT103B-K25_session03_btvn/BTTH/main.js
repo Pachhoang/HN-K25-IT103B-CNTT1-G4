f50037423f943054bc33bec60e3793f1cfe3907f
Nguyễn Tuấn Hưng <thwng08092007@gmail.com>
2026-01-30 10:03:48 +0700

    btvn

--- NguyenTuanHung_B25DTCN244_IT103B-K25_session03_btvn/BTTH/main.js
+++ NguyenTuanHung_B25DTCN244_IT103B-K25_session03_btvn/BTTH/main.js
@@ -1,11 +1,20 @@
 let password = "";
 password = prompt("Nhập password");
 let count = 3;
+let flag = true;
 while (password != "admin123"){
     if (count == 1){
+        flag = false;
         break;
     }
 
     password = prompt(`Vui lòng nhập lại password, bạn còn ${count-1} lần thử`);
     count--;
+}
+
+if (flag){
+    let choose;
+    while (choose != 3){
+        choose = +prompt("Nhập lựa chọn");
+    }
 }
