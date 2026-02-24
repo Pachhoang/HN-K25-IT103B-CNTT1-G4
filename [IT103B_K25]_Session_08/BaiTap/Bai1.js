4a821ea6e9872463120600005359305156066b12
Demon :) <siuotp@gmail.com>
2026-02-24 18:21:40 +0700

    Session08

---
+++ [IT103B_K25]_Session_08/BaiTap/Bai1.js
@@ -0,0 +1,15 @@
+const players = [
+    "Messi - Forward",
+    "Ronaldo - Forward",
+    "Neymar - Forward",
+    "De Bruyne - Midfielder",
+    "Kante - Midfilelder",
+    "Van Dijk - Defender",
+    "Alisson - Goalkeeper",
+];
+players.forEach(printPlayer);
+
+
+function printPlayer(nums){{
+    console.log(nums);
+}}
