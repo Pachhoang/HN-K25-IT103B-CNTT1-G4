55e6822ff1ea139ce24b9d824ee106ae85b7dbb1
Pachhoang <kiviyt2007@gmail.com>
2026-02-24 19:54:02 +0700

    btvn

---
+++ Bài 8/b1.js
@@ -0,0 +1,14 @@
+const players=[
+    "Messi-Forward",
+    "Ronaldo-Forward",
+    "Neymar-Forward",
+    "De Bruyne-Midfielder",
+    "Kante-Midfielder",
+    "Van Dijik-Defender",
+    "Alisson-Goldkeeper"
+]
+function displayPlayers(players) {
+    players.forEach(function(player) {
+        console.log(player);
+    });
+}
